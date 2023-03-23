dbcaa50995f1fa8ae69d3898882a725c2fb92a63
Iloke-Alusala <93942530+Iloke-Alusala@users.noreply.github.com>
2023-03-23 08:28:19 +0200

    Add files via upload

---
+++ Colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  final black0 = const Color(0xFF000000);
+  final black1 = const Color(0xFF2B2B2D);
+  final black2 = const Color(0xFF808080);
+  final red = const Color(0xFFCC4F59);
+  final grey0 = const Color(0xFFAAB0B6);
+  final grey1  = const Color(0xFFD7DEDE);
+  final grey2 = const Color(0xFFEFF2F1);
+  final white = const Color(0xFFFFFFFF);
+}
