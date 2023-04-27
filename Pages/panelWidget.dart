14e63ba3755c72f9e801e83e4045a81019738284
Iloke-Alusala <93942530+Iloke-Alusala@users.noreply.github.com>
2023-04-27 12:10:19 +0200

    Update panelWidget.dart

--- Pages/panelWidget.dart
+++ Pages/panelWidget.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:diarydark/Widgets/wideNote.dart';
-import 'package:flutter/material.dart';
 import 'package:diarydark/Colors/Colors.dart';
-import 'package:diarydark/Pages/note_page.dart';
-import 'package:diarydark/Pages/folder_view.dart';
-import 'package:diarydark/DB/folderDB.dart';
 import 'package:diarydark/Models/Folder.dart';
-import 'package:diarydark/Services/HeroDialogRoute.dart';
-import 'package:diarydark/Widgets/addFolderButton.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class panelWidget extends StatelessWidget {
@@ -16,12 +9,11 @@ class panelWidget extends StatelessWidget {
   PanelController panelController;
   final colors = AppColors();
   List<Folder> folders = [];
-  String _heroTag = "heroAddFolder";
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
+      borderRadius: const BorderRadius.vertical(top: Radius.circular(40)),
         child: Container(
           height: MediaQuery.of(context).size.height*0.7,
 
